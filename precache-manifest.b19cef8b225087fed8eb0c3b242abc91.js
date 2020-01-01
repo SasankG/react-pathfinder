115fbd401c39c0dd0241806088f3fca2e4b9f1bc
SasankG <sasankgraju@gmail.com>
2020-01-01 12:12:09 -0500

    Updates

---
+++ precache-manifest.b19cef8b225087fed8eb0c3b242abc91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de39bf8b28ea7c3c0e0884b2180ebb28",
+    "url": "/react-pathfinder/index.html"
+  },
+  {
+    "revision": "6f0d6bdf69f409e2d340",
+    "url": "/react-pathfinder/static/css/main.51040c61.chunk.css"
+  },
+  {
+    "revision": "97e4e230deed9848513e",
+    "url": "/react-pathfinder/static/js/2.40b03303.chunk.js"
+  },
+  {
+    "revision": "6f0d6bdf69f409e2d340",
+    "url": "/react-pathfinder/static/js/main.f198d1c9.chunk.js"
+  },
+  {
+    "revision": "88ab93002584c6bba85c",
+    "url": "/react-pathfinder/static/js/runtime-main.a278fd2c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-pathfinder/precache-manifest.f81a8df6ffeb1ee2ba56f5ac1ff031db.js"
+  "/react-pathfinder/precache-manifest.b19cef8b225087fed8eb0c3b242abc91.js"
 );
 
 self.addEventListener('message', (event) => {
