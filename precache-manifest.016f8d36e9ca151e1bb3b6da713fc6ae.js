3a6167dafb8e7cb68434c54bc082b2256ca11dbe
SasankG <sasankgraju@gmail.com>
2020-01-01 11:55:10 -0500

    Updates

---
+++ precache-manifest.016f8d36e9ca151e1bb3b6da713fc6ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb2654f779683b04ee680072d952e39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d91f110da020680904fb",
+    "url": "/static/css/main.51040c61.chunk.css"
+  },
+  {
+    "revision": "97e4e230deed9848513e",
+    "url": "/static/js/2.40b03303.chunk.js"
+  },
+  {
+    "revision": "d91f110da020680904fb",
+    "url": "/static/js/main.d832bd6a.chunk.js"
+  },
+  {
+    "revision": "ad64aec4a39dffa11ef2",
+    "url": "/static/js/runtime-main.b55d6c79.js"
+  }
+]);
